c5eb090e3fd931ea324b426c0f4783a72faebc1c
renukumar07 <renukumar90@gmail.com>
2023-07-18 00:52:41 +0530

    Renamed PropertiesEntity to Property

--- src/main/java/com/bookmygift/repository/PropertiesRepository.java
+++ src/main/java/com/bookmygift/repository/PropertiesRepository.java
@@ -1,12 +1,12 @@
 package com.bookmygift.repository;
 
-import com.bookmygift.entity.PropertiesEntity;
+import com.bookmygift.entity.Properties;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PropertiesRepository extends JpaRepository<PropertiesEntity, Long> {
+public interface PropertiesRepository extends JpaRepository<Properties, Long> {
 
-    Optional<PropertiesEntity> findByPropName(String propName);
+    Optional<Properties> findByPropName(String propName);
 
 }
